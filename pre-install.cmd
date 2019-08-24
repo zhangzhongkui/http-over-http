338e4955606b5d91831d2e559d64739a886a58cb
zhangzhongkui <tzhangshare@gmail.com>
2019-08-24 14:35:06 +0000

    apt-get packages

---
+++ pre-install.cmd
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt-get install autogen autoconf libtool
+sudo apt-get install g++
+sudo apt-get install libssl-dev
+sudo apt-get install libpcre3 libpcre3-dev
+./configure --prefix=/opt/ts --enable-experimental-plugins --enable-example-plugins
